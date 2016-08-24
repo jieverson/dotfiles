8b5eefc806abb660d8acff3eeddb0d1c6e26483f
Jiéverson Maissiat <jieverson@gmail.com>
2016-08-24 02:19:05 -0300

    add neovim to pac_install.sh

--- pac_install.sh
+++ pac_install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-pacman -S sudo coreutils zsh grep scrot tmux tree tar gcc make git nodejs npm mongodb wget curl vim ranger mplayer libcaca w3m screenfetch cowsay fortune-mod sl cmatrix doge lolcat xorg-server xorg-server-utils xorg-xinit bspwm sxhkd rxvt-unicode numlockx feh less dmenu chromium thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer poppler-glib pulseaudio xf86-input-synaptics
+pacman -S sudo coreutils zsh grep scrot tmux tree tar gcc make git nodejs npm mongodb wget curl vim neovim ranger mplayer libcaca w3m screenfetch cowsay fortune-mod sl cmatrix doge lolcat xorg-server xorg-server-utils xorg-xinit bspwm sxhkd rxvt-unicode numlockx feh less dmenu chromium thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer poppler-glib pulseaudio xf86-input-synaptics
 
 #AUR
 #apacman
