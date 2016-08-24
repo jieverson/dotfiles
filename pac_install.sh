138dc157a1bfc6fc5cd0ac678388018ad121d9f5
Jiéverson Maissiat <jieverson@gmail.com>
2016-08-23 23:23:30 -0300

    add new packages to pac_install.sh

--- pac_install.sh
+++ pac_install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-pacman -S sudo coreutils zsh grep scrot tmux tree tar gcc make git nodejs npm mongodb wget curl vim ranger w3m screenfetch cowsay fortune-mod sl cmatrix doge lolcat xorg-server xorg-server-utils xorg-xinit bspwm sxhkd rxvt-unicode numlockx feh less dmenu chromium thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer poppler-glib
+pacman -S sudo coreutils zsh grep scrot tmux tree tar gcc make git nodejs npm mongodb wget curl vim ranger mplayer libcaca w3m screenfetch cowsay fortune-mod sl cmatrix doge lolcat xorg-server xorg-server-utils xorg-xinit bspwm sxhkd rxvt-unicode numlockx feh less dmenu chromium thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer poppler-glib xf86-input-synaptics
 
 #AUR
 #apacman
